3e42a1264f8155d2d2a1c95709d947f2bc5e9f38
rick1310 <ricky.chance@gmail.com>
2020-10-27 14:07:55 -0500

    Added df_destroyer() to example_module

--- my_lambdata/example_module.py
+++ my_lambdata/example_module.py
@@ -6,7 +6,11 @@ import numpy as np
 
 LIST = ['Things', 'Inside', 'A List']
 
-def df_clean(df):
-    ###Cleans a dataframe###
-    # TODO - implement df_clean
+def df_clean():
+    """Cleans a dataframe"""
+    """TODO - implement df_clean"""
+    pass
+
+def df_destroyer():
+    """Destroys a dataframe"""
     pass
