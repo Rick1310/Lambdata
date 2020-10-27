0649ce111752e59fa5549740126d13094d0f547f
rick1310 <ricky.chance@gmail.com>
2020-10-27 16:33:23 -0500

    Created functions to handle data cleaning, jupyter notebood display options and some very simple multiplication.

--- my_lambdata/example_module.py
+++ my_lambdata/example_module.py
@@ -1,4 +1,4 @@
-###Lambdata - a collection of Data Science helepr functions###
+"""Lambdata - a collection of Data Science helepr functions"""
 
 # Libraries accessed through pipenv
 import pandas as pd
@@ -6,11 +6,34 @@ import numpy as np
 
 LIST = ['Things', 'Inside', 'A List']
 
-def df_clean():
+def df_clean(df):
     """Cleans a dataframe"""
-    """TODO - implement df_clean"""
-    pass
 
-def df_destroyer():
-    """Destroys a dataframe"""
-    pass
+
+    # Will identify columns with more than 500 observations. 
+
+    hc_cols = [for col in col in df.describe(include='object').columns
+               if df[col].nunique() > 500]
+
+    # Will take the identified columns and remove them from the df. 
+
+    df.drop(columns=hc_cols, inplace=True)
+
+    return df
+
+def df_display(df):
+    """sets notebook display option"""
+
+    # Will set the max rows allowed in a notebook to 999.
+    max_rows = pd.set_option("display.max_rows", 999)
+
+
+def enlarge(n):
+    """Multiplys a number by 100"""
+
+    return n*100
+
+    y = int(input("Enter a number"))
+    print(y, enlarge(y))
+
+
---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+    REQUIRED = ["numpy", "pandas"]
+
+setuptools.setup(
+    name="lambdata-rick1310", # Replace with your own username
+    version="0.0.1",
+    author="Rick1310",
+    author_email="author@example.com",
+    description="Collection of DS functions",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Rick1310/Lambdata",
+    packages=setuptools.find_packages(),
+    install_requires=REQUIRED,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
