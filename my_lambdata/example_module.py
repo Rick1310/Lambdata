38c9f7fe002d3d8f69fd754bb3410d8dbeeaa3ef
rick1310 <ricky.chance@gmail.com>
2020-10-27 13:27:09 -0500

    Created example_module

---
+++ my_lambdata/example_module.py
@@ -0,0 +1,12 @@
+****Lambdata - a collection of Data Science helepr functions***
+
+# Libraries accessed through pipenv
+import pandas as import pd
+import numpy as np
+
+LIST = ['Things', 'Inside', 'A List']
+
+def df_cleean:
+    ***Cleans a dataframe***
+    # TODO - implement df_clean
+    pass
