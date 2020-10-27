1622a499f1344e8be9e7d88a2167a4e8e49c1d64
rick1310 <ricky.chance@gmail.com>
2020-10-27 13:55:51 -0500

    New commit

--- my_lambdata/example_module.py
+++ my_lambdata/example_module.py
@@ -1,12 +1,12 @@
-****Lambdata - a collection of Data Science helepr functions***
+###Lambdata - a collection of Data Science helepr functions###
 
 # Libraries accessed through pipenv
-import pandas as import pd
+import pandas as pd
 import numpy as np
 
 LIST = ['Things', 'Inside', 'A List']
 
-def df_cleean:
-    ***Cleans a dataframe***
+def df_clean(df):
+    ###Cleans a dataframe###
     # TODO - implement df_clean
     pass
